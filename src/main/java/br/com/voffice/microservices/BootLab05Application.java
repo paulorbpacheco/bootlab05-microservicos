01081af171f994ee131a62de1aa2e9af39a09957
Paulo Pacheco <paulorb.pacheco@gmail.com>
2017-06-07 20:24:20 -0300

    Organização dos imports

--- src/main/java/br/com/voffice/microservices/BootLab05Application.java
+++ src/main/java/br/com/voffice/microservices/BootLab05Application.java
@@ -1,20 +1,13 @@
 package br.com.voffice.microservices;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import static springfox.documentation.builders.PathSelectors.regex;
-
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.service.ApiInfo;
 
 
 @SpringBootApplication
